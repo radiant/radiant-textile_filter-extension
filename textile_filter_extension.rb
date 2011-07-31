7473137610a452a3b471ac3ad9d9e28842fd5099
jsntv200 <jsntv200@gmail.com>
2011-07-31 11:26:00 +1000

    require RedCloth and declare as a config.gem

--- textile_filter_extension.rb
+++ textile_filter_extension.rb
@@ -1,7 +1,13 @@
+require "RedCloth"
+
 class TextileFilterExtension < Radiant::Extension
   version "1.0.0"
   description "Allows you to compose page parts or snippets using the Textile text filter."
   url "http://textism.com/tools/textile/"
+
+  extension_config do |config|
+    config.gem "RedCloth", :version => ">= 4.2.0"
+  end
 
   def activate
     TextileFilter
