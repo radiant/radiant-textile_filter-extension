b2c883a27aef8f04b3e385eb5fa64d5f77186a0c
johnmuhl <git@johnmuhl.com>
2011-08-17 13:50:25 -0500

    remove deprecated use of config.gem

--- textile_filter_extension.rb
+++ textile_filter_extension.rb
@@ -5,10 +5,6 @@ class TextileFilterExtension < Radiant::Extension
   description "Allows you to compose page parts or snippets using the Textile text filter."
   url "http://textism.com/tools/textile/"
 
-  extension_config do |config|
-    config.gem "RedCloth", :version => ">= 4.2.0"
-  end
-
   def activate
     TextileFilter
     Page.send :include, TextileTags
